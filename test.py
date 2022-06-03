0db88ef62553e9148a3825bb3f9743f4db09d2dc
KhamNguyen <khamnguyen@KhamNguyens-MacBook-Pro.local>
2022-06-03 10:42:18 +0700

    Add file test.py and app.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from operator import attrgetter
+
+
+student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
+
+for student, attendance in student_attendance.items():
+    print(f"{student}: {attendance}") 
